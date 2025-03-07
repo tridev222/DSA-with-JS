57e39c5e36eea3bb98112843ab9614644073ec57
Devesh Gangwar <gangward82@gmail.com>
2025-03-07 16:20:58 +0530

    leet_80

---
+++ remove_duplicate_2.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    if (nums.length <= 2) return nums.length;  
+
+    let i = 2;
+    for (let j = 2; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 2]) { 
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
